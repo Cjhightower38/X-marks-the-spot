2a606a60bc87e76550bcf6a6168717e063ab184c
Cornelius Jermaine Hightower <75323116+Cjhightower38@users.noreply.github.com>
2022-02-01 16:46:57 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+# 🚨 Don't change the code below 👇
+row1 = ["⬜️","⬜️","⬜️"]
+row2 = ["⬜️","⬜️","⬜️"]
+row3 = ["⬜️","⬜️","⬜️"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure? ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this row 👇
+
+# The horizontal position's in int form.
+row = int(position[0])
+
+# The verticle position's in int form.
+column = int(position[1])
+
+# In the map list go to this column or first nested list then enter the row or second nested listand place the string X.
+map[column - 1][row - 1] = 'X'
+
+
+#Write your code above this row 👆
+
+# 🚨 Don't change the code below 👇
+print(f"{row1}\n{row2}\n{row3}")
